1902d9b496b812c7635663225235dd2125d1019c
wconstain <wconstain@gmail.com>
2016-03-13 12:52:51 -0500

    recursos de idioma de la grilla

--- B2C/Controllers/AdministrationController.cs
+++ B2C/Controllers/AdministrationController.cs
@@ -173,5 +173,43 @@ namespace B2C.Controllers
                 }, JsonRequestBehavior.AllowGet);
             }
         }
+
+        #region Recursos de idioma
+        /// <summary>
+        /// Recursos de Idioma para Datatables.
+        /// </summary>
+        /// <returns>Lista con rescursos.</returns>
+        [HttpGet]
+        public JsonResult DatatablesLocalizacion()
+        {
+            return Json(new
+            {
+                sProcessing = "En proceso...",
+                sLengthMenu = "Mostrar _MENU_ registros",
+                sZeroRecords = "No se encontraron resultados",
+                sEmptyTable = "Ningún dato disponible en esta tabla",
+                sInfo = "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                sInfoEmpty = "Mostrando registros del 0 al 0 de un total de 0 registros",
+                sInfoFiltered = "(filtrado de un total de _MAX_ registros)",
+                sInfoPostFix = "",
+                sSearch = "Consultar",
+                sUrl = "",
+                sInfoThousands = ",",
+                sLoadingRecords = "Cargando...",
+                oPaginate = new
+                {
+                    sFirst = "Primero",
+                    sLast = "Último",
+                    sNext = "Siguiente",
+                    sPrevious = "Anterior"
+                },
+                oAria = new
+                {
+                    sSortAscending = "Activar para ordenar la columna de manera ascendente",
+                    sSortDescending = "Activar para ordenar la columna de manera descendente"
+                },
+            }, JsonRequestBehavior.AllowGet);
+        }
+        #endregion
     }
 }
