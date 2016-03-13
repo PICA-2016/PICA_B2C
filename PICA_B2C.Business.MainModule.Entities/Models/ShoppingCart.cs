5ac9ec619b169e4aac4f0c2168c359001c30838d
wconstain <wconstain@gmail.com>
2016-03-13 14:16:14 -0500

    carrito de compras

---
+++ PICA_B2C.Business.MainModule.Entities/Models/ShoppingCart.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PICA_B2C.Business.MainModule.Entities.Models
+{
+    /// <summary>
+    /// ShoppingCart entity.
+    /// </summary>
+    public class ShoppingCart
+    {
+        /// <summary>
+        /// Identifier.
+        /// </summary>
+        public int ShoppingCartId { get; set; }
+
+        /// <summary>
+        /// Product identifier.
+        /// </summary>
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Product.
+        /// </summary>
+        public virtual Product Product { get; set; }
+
+        /// <summary>
+        /// quantity of the product.
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
